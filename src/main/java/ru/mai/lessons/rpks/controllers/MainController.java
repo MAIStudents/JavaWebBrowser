e920d92e77851823f5b3beb618663502a3ded1cd
MaximusPokeZ <mzaslavtsev@gmail.com>
2024-11-23 17:53:21 +0300

    add favorites and fix some bugs

--- src/main/java/ru/mai/lessons/rpks/controllers/MainController.java
+++ src/main/java/ru/mai/lessons/rpks/controllers/MainController.java
@@ -1,21 +1,29 @@
 package ru.mai.lessons.rpks.controllers;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.web.WebEngine;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.LinkedHashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -23,6 +31,9 @@ import java.util.zip.ZipOutputStream;
 public class MainController implements Initializable {
   @FXML
   private TabPane tabPane;
+
+  @FXML
+  private HBox hBox;
 
   @FXML
   private TextField urlField;
@@ -40,16 +51,26 @@ public class MainController implements Initializable {
   private Button delButton;
 
   @FXML
+  public Button favoriteButton;
+  private final Set<String> favorites = new LinkedHashSet<>();
+
+  @FXML
   private MenuButton menuButton;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     addNewTab("https://www.google.com");
 
-    goButton.setOnAction(actionEvent -> loadPage());
+    goButton.setOnAction(actionEvent -> loadPageFromTextBar());
     reloadButton.setOnAction(actionEvent -> reloadPage());
     addButton.setOnAction(actionEvent -> addNewTab("https://www.google.com"));
     delButton.setOnAction(actionEvent -> delTab());
+    favoriteButton.setOnAction(actionEvent -> addToFavorites());
+
+    Platform.runLater(() -> {
+      Stage mainStage = (Stage) tabPane.getScene().getWindow();
+      mainStage.setOnCloseRequest(event -> closeApp());
+    });
 
     tabPane.getTabs().addListener((ListChangeListener.Change<?extends Tab> change) -> {
       if (tabPane.getTabs().isEmpty()) {
@@ -58,27 +79,56 @@ public class MainController implements Initializable {
     });
   }
 
-  private void loadPage() {
+  private void loadPageFromTextBar() {
     String input = urlField.getText().trim();
+    log.debug("{}", input);
     if (input.isEmpty()) {
+      log.warn("URL field is empty");
       return;
     }
 
     Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
     if (selectedTab != null) {
       PageTabController currentTab = (PageTabController) selectedTab.getUserData();
-      if (currentTab != null) {
-        String url = input.startsWith("http://") || input.startsWith("https://") ? input : "https://" + input;
-        currentTab.getWebEngine().getLoadWorker().exceptionProperty().addListener(((observableValue, oldValue, newValue) -> {
-          if (newValue != null) {
-            String googleSearchUrl = "https://www.google.com/search?q=" + input;
-            currentTab.getWebEngine().load(googleSearchUrl);
-          }
-        }));
-        currentTab.getWebEngine().load(url);
-      }
+      loadPage(input, currentTab);
+    } else {
+      log.warn("No selected tab to load the page");
     }
   }
+
+
+  private void loadPage(String url, PageTabController pageTabController) {
+    if (url == null || url.isEmpty()) {
+      log.warn("URL is empty or null");
+      return;
+    }
+    if (pageTabController == null) {
+      log.warn("Error with current tab");
+      return;
+    }
+
+    String formattedUrl = url.startsWith("http://") || url.startsWith("https://") ? url : "https://" + url;
+
+    pageTabController.getWebEngine().getLoadWorker().exceptionProperty().removeListener(pageTabController.getExceptionListener());
+
+    ChangeListener<Throwable> exceptionListener = (observableValue, oldException, newException) -> {
+      if (newException != null) {
+        log.error("Error loading URL: {}", formattedUrl, newException);
+
+        String googleSearchUrl = "https://www.google.com/search?q=" + url;
+        log.info("Redirecting to Google Search: {}", googleSearchUrl);
+        pageTabController.getWebEngine().load(googleSearchUrl);
+      }
+    };
+    pageTabController.setExceptionListener(exceptionListener);
+
+    pageTabController.getWebEngine().getLoadWorker().exceptionProperty().addListener(exceptionListener);
+
+    log.info("Loading URL: {}", formattedUrl);
+    pageTabController.getWebEngine().load(formattedUrl);
+  }
+
+
 
   private void reloadPage() {
     Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
@@ -95,13 +145,60 @@ public class MainController implements Initializable {
     Tab newTab = newTabController.getTab();
     tabPane.getTabs().add(newTab);
     tabPane.getSelectionModel().select(newTab);
+    log.info("Add new tab");
   }
 
   private void delTab() {
     Tab curentTab = tabPane.getSelectionModel().getSelectedItem();
     if (curentTab != null) {
       tabPane.getTabs().remove(curentTab);
+      log.info("Tab remove");
     }
+  }
+
+  private void addToFavorites() {
+    PageTabController pageTabController = (PageTabController) tabPane.getSelectionModel().getSelectedItem().getUserData();
+    if (pageTabController != null) {
+      String currentURL = pageTabController.getWebEngine().getLocation();
+      if (favorites.add(currentURL)) {
+        log.info("Added to favorites url: {}", currentURL);
+      } else {
+        log.info("This url is already in favorites {}", currentURL);
+      }
+    } else {
+      log.warn("No active tab to add to favorites");
+    }
+  }
+
+  @FXML
+  private void selectFavorites () {
+    if (favorites.isEmpty()) {
+      log.warn("No favorites to show");
+      return;
+    }
+
+    Stage favoriteStage = new Stage();
+    VBox favoriteList = new VBox(10);
+    favoriteList.setPadding(new Insets(10));
+    favoriteList.setAlignment(Pos.BOTTOM_LEFT);
+
+    for (String favorite : favorites) {
+      Button favoriteUrlButton = new Button(favorite);
+      PageTabController pageTabController = (PageTabController) tabPane.getSelectionModel().getSelectedItem().getUserData();
+      favoriteUrlButton.setOnAction(actionEvent -> loadPage(favorite, pageTabController));
+      favoriteList.getChildren().add(favoriteUrlButton);
+    }
+
+    ScrollPane scrollPane = new ScrollPane(favoriteList);
+    Scene scene = new Scene(scrollPane, 200, 300);
+    favoriteStage.setScene(scene);
+    favoriteStage.setTitle("Favorites");
+
+    Stage primaryStage = (Stage) tabPane.getScene().getWindow();
+    favoriteStage.setX(primaryStage.getX() + primaryStage.getWidth() - scene.getWidth());
+    favoriteStage.setY(primaryStage.getY() + tabPane.getHeight() / 2);
+
+    favoriteStage.show();
   }
 
   @FXML
@@ -169,8 +266,18 @@ public class MainController implements Initializable {
     }
   }
 
+  @FXML
+  public void goBack(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  public void goForward(ActionEvent actionEvent) {
+  }
+
   private void closeApp() {
     log.info("Closing application...");
     Platform.exit();
+
   }
+
 }
--- src/main/java/ru/mai/lessons/rpks/controllers/PageTabController.java
+++ src/main/java/ru/mai/lessons/rpks/controllers/PageTabController.java
@@ -1,5 +1,7 @@
 package ru.mai.lessons.rpks.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -10,11 +12,18 @@ public class PageTabController {
   private final Tab tab;
   private final WebView webView;
   private final WebEngine webEngine;
+  private ChangeListener<Throwable> listener;
 
   public PageTabController (String url) {
     this.webView = new WebView();
     this.webEngine = webView.getEngine();
     this.webEngine.load(url);
+    this.listener = (observableValue, throwable, t1) -> {
+      if (t1 != null) {
+        String googleSearchUrl = "https://www.google.com/search?q=" + url;
+        webEngine.load(googleSearchUrl);
+      }
+    };
 
     VBox.setVgrow(webView, Priority.ALWAYS);
     VBox vBox = new VBox(webView);
@@ -35,4 +44,11 @@ public class PageTabController {
     return webView;
   }
 
+  public ChangeListener<Throwable> getExceptionListener() {
+    return listener;
+  }
+
+  public void setExceptionListener(ChangeListener<Throwable> exceptionListener) {
+    this.listener = exceptionListener;
+  }
 }
