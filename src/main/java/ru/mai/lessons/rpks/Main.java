04f3e715a793f5420267dc0664cff2c62c7fbe56
ykwais <fedorkovaleks04@gmail.com>
2024-11-29 00:15:55 +0300

    settings overcome

--- src/main/java/ru/mai/lessons/rpks/Main.java
+++ src/main/java/ru/mai/lessons/rpks/Main.java
@@ -1,7 +1,23 @@
 package ru.mai.lessons.rpks;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main_window.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX Web Browser");
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
